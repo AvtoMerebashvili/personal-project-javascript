9cfc82d39a840d95b10f106ee0b9f5e03d68ad1c
Avtandil-1 <avtandil.merebashvili.2@gmail.com>
2021-08-16 11:30:25 +0400

    feat(personal-project-javascript): Final commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-class Transaction {
+import {Validator} from './validation.js'
+export class Transaction {
     logs = [];
     #scenarioInfo = {
         status: true,
@@ -94,6 +95,10 @@ class Transaction {
                                 error: null
                             }); 
                         scenarioInfo.status = true;
+                        if(scenario.indexOf(step) == scenario.length-1){
+                            this.logs = [];
+                            this.store = {}
+                        }
                     }catch(err){
                         scenarioInfo.status = false;
                         if(typeof step === 'object' && !Array.isArray(step)){
@@ -200,87 +205,3 @@ class Transaction {
     }
 
 }
-
-class Validator {
-    static scenario(scenario) {
-        if (!Array.isArray(scenario)) throw new TypeError("scenario must be an array, with Objects inside");
-    }
-
-    static step(step,scenario){
-        if(typeof step === 'object' && !Array.isArray(step)){
-             if (step.index == undefined) throw new Error("There isn't index property");
-                else{
-                    if(typeof step.index != 'number') throw new Error('Type of index is not a number')
-                    if(scenario.indexOf(step)==0 && scenario[scenario.indexOf(step)].index != 1) throw new Error('there is not index 1 in steps')
-                }
-
-            if (step.call == undefined)  throw new Error("there is not call method");
-                else{
-                    if(typeof step.call != 'function') throw new TypeError('type of call is not function')
-                }
-
-            if (step.meta == undefined)  throw new Error("there is not meta property");
-                else if (step.meta != undefined){
-                    if(typeof step.meta != 'object' || Array.isArray(step.meta)) throw new TypeError('type of meta is not object')
-                    else{
-                        if (step.meta.title == undefined) throw new Error("there is not title");
-                        else{
-                            if(typeof step.meta.title != 'string') throw new TypeError('type of title is not string')
-                        }
-                        if(step.meta.description == undefined)  throw new Error("there is not description");
-                        else{
-                            if(typeof step.meta.description != 'string') throw new TypeError('description of title is not string')
-                        }
-                    } 
-                }
-            if(scenario.indexOf(step) != 0){
-                if(scenario[scenario.indexOf(step)].index == scenario[scenario.indexOf(step)-1].index)throw new Error(`this step has same index as before step ${scenario.indexOf(step)}`)
-                else if(scenario[scenario.indexOf(step)].index - scenario[scenario.indexOf(step)-1].index != 1){
-                    throw new TypeError(`there is broken chain on ${scenario.indexOf(step)+1}`)
-                }
-            }
-        }else if(typeof step === undefined){
-            throw new TypeError(`this step is not object`)
-        }else{
-            throw new TypeError(`there is broken chain on ${scenario.indexOf(step)+1}`)
-        }
-    }
-}
-
-const scenario = [
-  {
-    index: 1,
-    meta: {
-      title: "Read popular customers",
-      description:
-        "This action is responsible for reading the most popular customers",
-    },
-    // callback for main execution
-    call: async (store) => {},
-    // callback for rollback
-    restore: async (store) => {},
-  },
-  {
-    index: 2,
-    meta: {
-        title: 'Delete customer',
-        description: 'This action is responsible for deleting customer',
-    },
-    // callback for main execution
-    call: async (store) => {},
-    // callback for rollback
-    restore: async (store) => {},
-  },
-];
-
-const transaction = new Transaction();
-
-(async () => {
-    try {
-        await transaction.dispatch(scenario);
-        const store = transaction.store; // {} | null
-        const logs = transaction.logs; // []    
-    } catch (err) {
-        console.log(err);
-    }
-})();
---
+++ main.js
@@ -0,0 +1,39 @@
+import {Transaction} from './index.js'
+
+const scenario = [
+  {
+    index: 1,
+    meta: {
+      title: "Read popular customers",
+      description:
+        "This action is responsible for reading the most popular customers",
+    },
+    // callback for main execution
+    call: async (store) => {},
+    // callback for rollbacke
+    restore: async (store) => {},
+  },
+  {
+    index: 2,
+    meta: {
+        title: 'Delete customer',
+        description: 'This action is responsible for deleting customer',
+    },
+    // callback for main execution
+    call: async (store) => {},
+    // callback for rollback
+    restore: async (store) => {},
+  },
+];
+
+const transaction = new Transaction();
+
+(async () => {
+    try {
+        await transaction.dispatch(scenario);
+        const store = transaction.store; // {} | null
+        const logs = transaction.logs; // []  
+    } catch (err) {
+        console.log(err);
+    }
+})();
